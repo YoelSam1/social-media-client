52951ae4f5bca893b5031a0b0f61f51d28bfd56a
Eamith <yoesam36962@stud.noroff.no>
2024-02-13 14:30:38 +0100

    final update for logout.test.js

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,22 +1,39 @@
-/* eslint-disable no-undef */
 import { logout } from "./logout.js";
+import { remove } from "../../storage/index.js";
 
 // Mock
+jest.mock("../../storage/index.js", () => ({
+  remove: jest.fn(),
+}));
+
+// Mock localStorage
 const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
   removeItem: jest.fn(),
 };
 
+// eslint-disable-next-line no-undef
 global.localStorage = localStorageMock;
 
-describe("logout function", () => {
+describe("logout", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("should clear the token from the browser", () => {
+  it("clear the token from local storage", () => {
+    // Simulate logged-in state by setting a token in storage
+    localStorageMock.setItem("token", "dummyToken");
+    localStorageMock.setItem(
+      "profile",
+      JSON.stringify({ email: "yes@noroff.no" }),
+    );
+
+    // Call logout function
     logout();
 
-    expect(localStorageMock.removeItem).toHaveBeenCalledWith("token");
-    expect(localStorageMock.removeItem).toHaveBeenCalledWith("profile");
+    // Verify if remove function is called with "token" as argument
+    expect(remove).toHaveBeenCalledWith("token");
+    expect(remove).toHaveBeenCalledWith("profile");
   });
 });
